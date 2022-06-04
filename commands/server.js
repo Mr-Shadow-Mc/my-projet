ed5bd3cc588d090d0f1ef67492ceb2ff0d8d943f
Mr-Shadow-Mc <82217376+Mr-Shadow-Mc@users.noreply.github.com>
2022-06-04 23:45:04 +0200

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -11,13 +11,7 @@ module.exports = {
         .addField('General', [
             `**Name:** ${message.guild.name}`,
             `**ID:** ${message.guild.id}`,
-            `**Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
-            `**Region:** ${regions[message.guild.region]}`,
-            `**Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
-            `**Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
-            `**Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
-            `**Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} [${moment(message.guild.createdTimestamp).fromNow()}]`,
-            '\u200b'
+            `**Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`
         ])
     },
     name: 'server',
