c8e0f645b4c5a7781ba83c706f72019fcce210ec
Mr-Shadow-Mc <82217376+Mr-Shadow-Mc@users.noreply.github.com>
2022-06-04 23:28:45 +0200

    Create server.js

---
+++ commands/server.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const Discord = require('discord.js')
+const config = require('../config.json')
+
+module.exports = {
+    run: async (message, args, client) => {
+        message.channel.send(new Discord.MessageEmbed()
+        .setColor('#fff100')
+        .setDescription(message.guild.name)
+        )
+    },
+    name: 'server',
+    guildOnly: true,
+    help: {
+        description: '',
+        syntax: ''
+    }
+}
